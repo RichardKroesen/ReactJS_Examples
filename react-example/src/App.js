41af274b2855e5b36b43000f108190c9f802a3c1
Richard Kroesen <kroesen.ri@gmail.com>
2023-09-23 01:26:57 +0200

    Enh blogposting system.

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,7 +1,72 @@
-import Message from "./TestMessage"
+import React, { Component } from 'react';
 
-function App() {
-  return <div><Message /></div>
+class BlogPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+      posts: [],
+    };
+  }
+
+  handleTitleChange = (event) => {
+    this.setState({ title: event.target.value });
+  };
+
+  handleContentChange = (event) => {
+    this.setState({ content: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+      title: this.state.title,
+      content: this.state.content,
+    };
+    this.setState({
+      title: '',
+      content: '',
+      posts: [...this.state.posts, newPost],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>My Blog</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              id="title"
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="content">Content:</label>
+            <textarea
+              id="content"
+              value={this.state.content}
+              onChange={this.handleContentChange}
+            ></textarea>
+          </div>
+          <button type="submit">Publish</button>
+        </form>
+        <div>
+          <h2>Recent Posts</h2>
+          {this.state.posts.map((post, index) => (
+            <div key={index}>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default BlogPost;
